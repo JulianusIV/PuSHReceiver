9f1d085514d2c53ddd2634b32a0fa47435e31e06
JulianusIV <ivjulianus@gmail.com>
2022-06-02 19:29:25 +0200

    Implemeted LeaseService

--- ServiceLayer/LeaseService.cs
+++ ServiceLayer/LeaseService.cs
@@ -1,13 +1,39 @@
 ﻿using Data.JSONObjects;
+using Plugins.Interfaces;
+using PubSubHubBubReciever;
 using Services;
+using System.Runtime.CompilerServices;
+using Timer = System.Timers.Timer;
 
 namespace ServiceLayer
 {
     public class LeaseService : ILeaseService
     {
+        private static Dictionary<ulong, Timer> _leaseTimers { get; } = new();
+
+        [MethodImpl(MethodImplOptions.Synchronized)]
         public void RegisterLease(DataSub dataSub, int leaseTime)
         {
-            throw new NotImplementedException();
+            Console.WriteLine($"Scheduling lease renewal for topic {dataSub.TopicID} in {leaseTime} seconds ({TimeSpan.FromSeconds(leaseTime).TotalDays} days)");
+
+            var timer = GetTimer(dataSub);
+            timer.Stop();
+            timer.Interval = TimeSpan.FromSeconds(leaseTime).TotalMilliseconds;
+            timer.AutoReset = false;
+            timer.Start();
+        }
+
+        private Timer GetTimer(DataSub dataSub)
+        {
+            if (_leaseTimers.ContainsKey(dataSub.TopicID))
+                return _leaseTimers[dataSub.TopicID];
+
+            var timer = new Timer();
+            timer.Elapsed += async (sender, e)
+                => await Runtime.Instance.PluginLoader.ResolvePlugin<IConsumerPlugin>(dataSub.FeedConsumer)
+                    .SubscribeAsync(dataSub);
+            _leaseTimers.Add(dataSub.TopicID, timer);
+            return timer;
         }
     }
 }

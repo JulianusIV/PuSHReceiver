0b4ed4f8e9971139e2bfdee1206e8fc10c30e180
JulianusIV <ivjulianus@gmail.com>
2023-03-14 14:36:09 +0100

    improve logging for request type

--- Models/ApiCommunication/Request.cs
+++ Models/ApiCommunication/Request.cs
@@ -1,9 +1,23 @@
-﻿namespace Models.ApiCommunication
+﻿using System.Reflection.PortableExecutable;
+
+namespace Models.ApiCommunication
 {
     public class Request
     {
         public Dictionary<string, string> Headers { get; set; } = new();
         public Dictionary<string, string> QueryParameters { get; set; } = new();
         public string? Body { get; set; }
+
+        public override string ToString()
+        {
+            var ret = "Headers:\n";
+            foreach (var header in Headers)
+                ret += $"{header.Key}: {header.Value}\n";
+            ret += "Params:\n";
+            foreach (var param in QueryParameters)
+                ret += $"{param.Key}: {param.Value}\n";
+            ret += $"Body:\n{Body}";
+            return ret;
+        }
     }
 }
